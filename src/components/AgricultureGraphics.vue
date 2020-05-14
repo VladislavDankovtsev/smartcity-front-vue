096cdadd647fe62ac563a81e62dc7fbe83e32848
Vldaislav_19 <vladislav.dankovtsev@yandex.ru>
2020-05-15 01:39:54 +0300

    добавление графика температуры в сельском хозяйстве

--- src/components/AgricultureGraphics.vue
+++ src/components/AgricultureGraphics.vue
@@ -3,6 +3,7 @@
          align-items: center;
           min-width: 500px">
     <D3LineChart :config="chart_config" :datum="agricultures"></D3LineChart>
+    <D3LineChart :config="chart_config1" :datum="agricultures"></D3LineChart>
   </div>
 </template>
 
@@ -55,6 +56,48 @@ export default {
         },
         tooltip: {
           labels: { label: ['humiditySM', 'humidityTaH'] }
+        },
+        transition: {
+          duration: 350,
+          ease: 'easeQuadOut'
+        }
+      },
+      chart_config1: {
+        date: {
+          key: 'time',
+          inputFormat: '%Y-%m-%dT%H:%M:%S.%f',
+          outputFormat: '%Y-%m-%d'
+        },
+        values: ['temperature'],
+        axis: {
+          yTitle: 'Градусы',
+          xTitle: 'время',
+          yFormat: '.0f',
+          xFormat: '%H:%M',
+          yTicks: 5,
+          xTicks: 4
+        },
+        color: {
+          key: false,
+          keys: false,
+          scheme: 'schemeCategory10',
+          current: 'red',
+          default: '#AAA',
+          axis: '#000'
+        },
+        curve: 'curveBasis',
+        margin: {
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 40
+        },
+        points: {
+          visibleSize: 3,
+          hoverSize: 6
+        },
+        tooltip: {
+          labels: { label: ['temperature'] }
         },
         transition: {
           duration: 350,
